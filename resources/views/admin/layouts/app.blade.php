a57800ab27022f3b65bd9f44a84c2ce26a68328f
jiangbo1994925 <jiangbo1994925@gmail.com>
2019-04-28 12:11:22 +0800

    创建布局模板 导入资源文件

---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title', 'Laravel Blog') - 后台管理</title>
+    <!-- 样式 -->
+    <link href="{{ mix('css/admin.css') }}" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper" id="app" >
+
+    @include('admin.layouts._header')
+    @include('admin.layouts._slidebar')
+    <div class="content-wrapper">
+        <section class="content-header">
+            <h1>
+                Dashboard
+                <small>Control panel</small>
+            </h1>
+            <ol class="breadcrumb">
+                <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+                <li class="active">Dashboard</li>
+            </ol>
+        </section>
+
+        <section class="content">
+            @yield('content')
+        </section>
+    </div>
+    @include('admin.layouts._footer')
+</div>
+<!-- JS 脚本 -->
+<script src="{{ mix('js/admin.js') }}"></script>
+@yield('scriptAfterJs')
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,18 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('admin', function () {
+    return view('admin_template');
+});
+
+// 后台登录页面
+Route::namespace('Admin')->prefix('admin')->group(function () {
+    Route::redirect('/', url('admin/login/index'));
+    Route::prefix('login')->group(function () {
+        // 登录页面
+        Route::get('index', 'LoginController@index');
+        // 退出
+        Route::get('logout', 'LoginController@logout');
+    });
+});
