7d7f60e32f7c20743e7bb1cee77a2b495f44a141
jiangbo1994925 <jiangbo1994925@gmail.com>
2019-05-09 16:54:46 +0800

    标签表增加字段 文章数统计

--- database/seeds/TagsTableSeeder.php
+++ database/seeds/TagsTableSeeder.php
@@ -15,6 +15,7 @@ class TagsTableSeeder extends Seeder
             [
                 'id'=>1,
                 'name'=>'PHP',
+                'article_count'=>1,
                 'created_at' => '2019-4-30 19:39:12',
                 'updated_at' => '2019-4-30 16:39:12',
                 'deleted_at' => null
@@ -22,6 +23,7 @@ class TagsTableSeeder extends Seeder
             [
                 'id'=>2,
                 'name'=>'Laravel',
+                'article_count'=>1,
                 'created_at' => '2019-4-30 19:39:12',
                 'updated_at' => '2019-4-30 16:39:12',
                 'deleted_at' => null
@@ -29,6 +31,7 @@ class TagsTableSeeder extends Seeder
             [
                 'id'=>3,
                 'name'=>'JavaScript',
+                'article_count'=>1,
                 'created_at' => '2019-4-30 19:39:12',
                 'updated_at' => '2019-4-30 16:39:12',
                 'deleted_at' => null
@@ -36,6 +39,7 @@ class TagsTableSeeder extends Seeder
             [
                 'id'=>4,
                 'name'=>'Linux',
+                'article_count'=>1,
                 'created_at' => '2019-4-30 19:39:12',
                 'updated_at' => '2019-4-30 16:39:12',
                 'deleted_at' => null
@@ -43,6 +47,7 @@ class TagsTableSeeder extends Seeder
             [
                 'id'=>5,
                 'name'=>'Vue',
+                'article_count'=>1,
                 'created_at' => '2019-4-30 19:39:12',
                 'updated_at' => '2019-4-30 16:39:12',
                 'deleted_at' => null
